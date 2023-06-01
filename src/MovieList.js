16fee3f0259519080bc456ed076528c8eee429c4
NITHISHguviii <nithishkumarv0612@gmail.com>
2023-06-01 11:07:20 +0530

    capestone

---
+++ src/MovieList.js
@@ -0,0 +1,65 @@
+import { Movie } from './Movie';
+import {useState , useEffect } from 'react';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { useNavigate } from 'react-router-dom';
+import { API } from './global';
+
+export function MovieList() {
+
+    const [movieList, setMovieList] = useState([]);
+
+    const getMovies =() => {
+        fetch(`${API}/Movies`,{
+            method : "GET" , 
+        })
+        .then(data => data.json())
+        // .then(mvs => console.log(mvs));
+        .then(mvs => setMovieList(mvs)); 
+      };
+
+    useEffect(()=> getMovies(), []);
+
+const deleteMovie = (id) =>{
+ {/*console.log("Deleting Moviee..... ",id)  */}
+ //Delete -> Refresh data (refetch)
+ fetch(`${API}/Movies/${id}`,{method:"DELETE",}).then (() => getMovies()); // to avoid reloading the page after deletion
+};
+
+const navigate = useNavigate();
+
+    return (
+        <div>
+            <div className="movie-list">
+                {movieList.map((mv) => (
+                    <Movie 
+                    movie={mv} 
+                    deleteButton={
+                    <IconButton 
+                    color="error"
+                    onClick={()  => deleteMovie(mv.id)}
+                    aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton>
+                    }
+
+                    editButton={
+                        <IconButton 
+                        color="secondary"
+                        onClick={()  => navigate(`/movies/edit/${mv.id}`)}
+                        aria-label="edit">
+                            <EditIcon />
+                        </IconButton>
+                        }/>
+
+
+
+                ))}
+            </div>
+        </div>
+    );
+}
+
+
+// movies/edit/101   101 is(id)
---
+++ src/global.js
@@ -0,0 +1,2 @@
+ //export const API=`https://644b914d17e2663b9df2866c.mockapi.io`
+ export const API="https://pinterset-clone123.onrender.com"
